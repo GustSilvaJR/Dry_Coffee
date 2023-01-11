992a2546827ce29a0e4771f06fc058c79693d013
Gustavo Alessandro <gust.offic@gmail.com>
2023-01-11 16:49:58 -0300

    utilizando meu guard para validar se o usuario ainda está logado, para que caso acesse o login ja estando autenticado, redirecionar para a pagina dashboard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,14 +4,16 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { LoginComponent } from './core/auth/login/login.component';
-import { ForgotPasswordComponent } from './core/auth/forgot-password/forgot-password.component';
+import { LoginComponent } from './core/components/auth/login/login.component';
+import { ForgotPasswordComponent } from './core/components/auth/forgot-password/forgot-password.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes =[
   {
     path: '',
     component: LoginComponent,
     pathMatch: 'full',
+    canActivate:[AuthGuard]
   }, {
     path: '',
     component: AdminLayoutComponent,
