fb48f67251bd967e8d9fa806dfca08c3e4ccdc16
GustSilvaJR <gust.offic@gmail.com>
2023-01-19 15:56:34 -0300

    Ajustes de interface | Subscrevendo no serviço que irá efetuar o login do usuário e criando tratamento de erro para problemas no servidor ou e-mail/senha incorretas

--- src/app/core/components/auth/login/login.component.ts
+++ src/app/core/components/auth/login/login.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SignInService } from 'app/core/services/signIn/sign-in.service';
+import { Md5 } from 'ts-md5';
+
+import { LoginDTO } from 'app/core/interfaces/loginDTO';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
+export class LoginComponent implements OnInit {
 
   public msgError: string = "";
 
-  constructor(private formBuilder: FormBuilder, private signInService:SignInService) { }
+  constructor(private formBuilder: FormBuilder, private signInService: SignInService) { }
 
   ngOnInit(): void {
   }
@@ -23,13 +27,29 @@ export class LoginComponent implements OnInit {
   });
 
   public submitLogin(): void {
-    if (true) {
-      this.signInService.signIn();
-      alert("Formulário enviado com sucesso!");
-      this.msgError = "";
-    } else {
-      this.msgError = "E-mail ou senha incorretos!";
-    }
-  }
 
+    const dataLogin: LoginDTO = {
+      email: this.formLogin.get('email').value,
+      password: Md5.hashStr(this.formLogin.get('password').value),
+    };
+
+    this.signInService.signIn(dataLogin).subscribe({
+      next: (res) => {
+
+        if (!res.auth) {
+          alert("Formulário enviado com sucesso!");
+          this.msgError = res.msg;
+        } else {
+          this.msgError = '';
+        }
+
+      },
+      error: (err) => {
+        console.log("Error: ");
+        console.log(err);
+
+        this.msgError = "Servidor com problemas! Entre em contato com o administrador ou tente novamente mais tarde.";
+      }
+    })
+  }
 }
