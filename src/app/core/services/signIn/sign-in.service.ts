20a12701a5103a44c2dfe7a43bb960c5b4b7554b
GustSilvaJR <gust.offic@gmail.com>
2023-01-19 16:08:12 -0300

    Implementando serviço que faz o consumo da api no back end para validar o login do usuario

--- src/app/core/services/signIn/sign-in.service.ts
+++ src/app/core/services/signIn/sign-in.service.ts
@@ -1,14 +1,36 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+//Observable
+import { Observable} from 'rxjs';
+
+import { LoginDTO } from 'app/core/interfaces/loginDTO';
+
+@Injectable()
+
 export class SignInService {
 
-  constructor() { }
+  private url:string = "http://localhost:8000/login";
 
-  public signIn():void{
-    console.log("Login efetuado a partir do service");
+  constructor(private httpClient: HttpClient) { }
+
+  public signIn({email, password}):Observable<any>{
+    const data:LoginDTO = {
+      email,
+      password
+    };
+
+    return this.httpClient.post<boolean>(this.url, data).pipe(
+      res => {
+        console.log(res);
+        return res;
+      },
+      error => {
+        console.log(error);
+        return error;
+      }
+    )
   }
 
 }
