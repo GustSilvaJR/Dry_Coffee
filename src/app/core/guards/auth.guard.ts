004480a7c3319379694126610926c86f67fa7a60
GustSilvaJR <gust.offic@gmail.com>
2023-01-24 11:41:02 -0300

    Implementando serviço de autenticação com base na validação do token feita na função isAutenticated e caso não autenticado, retornando o usuário para a tela de login

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+
+//Services
+import { SignInService } from '../services/signIn/sign-in.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private signInService: SignInService, private router: Router) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    
+    if (!this.signInService.isAuthenticated()) {
       
-    return true;
+      this.router.navigate(['']);
+      console.log("Recusado");
+      return false;
+    } else {
+      console.log("Autenticado");
+      return true;
+    }
   }
 
 }
