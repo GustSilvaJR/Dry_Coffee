68c89f1804a4c1f595da86259c3f9ff08aa536a5
GustSilvaJR <gust.offic@gmail.com>
2023-01-26 16:09:14 -0300

    Organização de arquivos

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { HomeComponent } from '../../home/home.component';
-import { UserComponent } from '../../user/user.component';
-
-export const AdminLayoutRoutes: Routes = [
-    { path: 'dashboard',      component: HomeComponent },
-    { path: 'user',           component: UserComponent },
-];
