5990d951afe90e17d6b03455618db43be2d70dfc
GustSilvaJR <gust.offic@gmail.com>
2023-02-09 07:36:36 -0300

    Implementando lazy load no acesso ao componente terreiro

--- src/app/pages/terreiro/terreiro.module.ts
+++ src/app/pages/terreiro/terreiro.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Componentes
 import { TabelaTerreiroComponent } from './components/tabela-terreiro/tabela-terreiro.component';
+import { RouterModule } from '@angular/router';
+import { TerreiroRoutes } from './terreiro.routing';
 
 @NgModule({
   declarations: [
     TabelaTerreiroComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(TerreiroRoutes),
   ]
 })
 export class TerreiroModule { }
---
+++ src/app/pages/terreiro/terreiro.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { TabelaEntradasComponent } from '../entrada/components/tabela-entradas/tabela-entradas.component';
+import { HomeComponent } from '../home/home.component';
+import { UserComponent } from '../user/user.component';
+import { TabelaTerreiroComponent } from './components/tabela-terreiro/tabela-terreiro.component';
+
+export const TerreiroRoutes: Routes = [
+    
+];
