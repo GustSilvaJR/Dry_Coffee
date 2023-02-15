b04786c3fa25ab48161b346524a4528e7981f4c7
GustSilvaJR <gust.offic@gmail.com>
2023-02-15 11:17:44 -0300

    Criando componente de tabela geral para apresentação de dados

---
+++ src/app/pages/lavador/components/tabela-lavador/tabela-lavador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-lavador',
+  templateUrl: './tabela-lavador.component.html',
+  styleUrls: ['./tabela-lavador.component.scss']
+})
+export class TabelaLavadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
